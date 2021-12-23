5ad49a8d50abe255f338f8a20677cf0e14d6197e
Tasos Papalyras <tasos@rdnt.dev>
2021-12-24 00:27:17 +0200

    delete vendor

--- vendor/github.com/kirides/screencapture/d3d/iunknown_vtbl.go
+++
@@ -1,9 +0,0 @@
-package d3d
-
-type iUnknownVtbl struct {
-	// every COM object starts with these three
-	QueryInterface uintptr
-	AddRef         uintptr
-	Release        uintptr
-	// _QueryInterface2 uintptr
-}
--- vendor/github.com/lithammer/shortuuid/v3/go.mod
+++
@@ -1,5 +0,0 @@
-module github.com/lithammer/shortuuid/v3
-
-require github.com/google/uuid v1.2.0
-
-go 1.13
